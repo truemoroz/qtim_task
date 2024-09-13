e5b1f8046d8f5236cc29cd82b4a7e7cff1a97eab
pmorozov <moroz0@gmail.com>
2024-09-13 13:34:47 +0700

    refactor NestJsConsoleTransport.ts

--- src/common/lib/log/transports/console/NestJsConsoleTransport.ts
+++ src/common/lib/log/transports/console/NestJsConsoleTransport.ts
@@ -1,4 +1,4 @@
-import { ConsoleLogger } from '@nestjs/common'
+import { ConsoleLogger, LogLevel } from '@nestjs/common'
 import { LogMessagePayload } from '@/common/lib/log/models/LogMessagePayload'
 import { LogTransportBase } from '@/common/lib/log/transports/base/LogTransportBase'
 import { LogTransportType } from '@/common/lib/log/enum/LogTransportType'
@@ -19,12 +19,12 @@ export class NestJsConsoleTransport extends LogTransportBase {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { message, context, level, labels, stack, firstLine, ...rest } = info
 
-        const nestLevel = (level as string) === 'info' ? 'log' : level
+        const nestLevel = level === 'info' as LogLevel ? 'log' : level
 
         let resultMessage
         if (Object.keys(rest).length > 0 && message) {
             try {
-                resultMessage = message + ' ' + JSON.stringify(rest)
+                resultMessage = `${message} ${JSON.stringify(rest)}`
             } catch (err) {
                 console.log(err)
             }
@@ -34,10 +34,12 @@ export class NestJsConsoleTransport extends LogTransportBase {
             resultMessage = rest
         }
 
-        if (firstLine !== false) {
-            this.consoleLogger[nestLevel](resultMessage, context)
-        } else {
-            this.consoleLogger[nestLevel](resultMessage, '~ ' +  context)
+        const contextForLog = firstLine === true ? `~ ${context}` : context
+        if (nestLevel === 'error') {
+            this.consoleLogger.error(resultMessage, stack, contextForLog)
+            return
         }
+
+        this.consoleLogger[nestLevel](resultMessage, contextForLog)
     }
 }
